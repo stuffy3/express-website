4b2c02e8e49641a040387624f55f34819b7f9877
stuffy3 <tannerstufflebeam@gmail.com>
2022-01-06 14:02:51 -0700

    added express and nodemon

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
+// const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
+
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/',(req, res) => {
     res.send('Hello')
 })
 
 app.listen(3000,() => {
-    console.log('Docked on 3000')
-})
+    debug(`docked on port 3000`)
+});
