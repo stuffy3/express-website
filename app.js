18cfae9a6e4fe11ac2fb057ec659116e09f53137
stuffy3 <tannerstufflebeam@gmail.com>
2022-01-06 16:17:05 -0700

    creating sessions page

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-
+const sessionsRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -14,6 +14,19 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+sessionsRouter.route('/')
+    .get((req, res ) => {
+        res.render('sessions', {
+            sessions: [
+                {title: 'Session 1', description: 'this is session 1'},
+                {title: 'Session 2', description: 'this is session 2'},
+                {title: 'Session 3', description: 'this is session 3'},
+                {title: 'Session 4', description: 'this is session 4'},
+             ],
+        })
+    })
+
+app.use('/sessions', sessionsRouter)
 app.get('/',(req, res) => {
     res.render('index', {title: 'Globalmantics', data: ['a', 'b' ,'c']})
 })
